2c61092e752a5aaff6d5fb8e9c38d0e3f54f3c6a
theGpoint7 <griffioen.jesse@gmail.com>
2024-03-21 20:54:38 +0100

    new spring boot project

---
+++ src/main/java/novi/backend/opdracht/backendservice/BackendServiceUniqueThreadsApplication.java
@@ -0,0 +1,13 @@
+package novi.backend.opdracht.backendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServiceUniqueThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServiceUniqueThreadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novi/backend/opdracht/backendservice/BackendServiceUniqueThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.backend.opdracht.backendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceUniqueThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
